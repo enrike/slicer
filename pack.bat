ec802b36dbce8d7ee140fa7144166449e73a8f5f
enrike <enrikehurtadomendieta@gmail.com>
2013-12-29 14:49:58 +0100

    new pyinstaller system for linux

--- pack.bat
+++
@@ -1,8 +0,0 @@
-C:\Python27\python.exe -O "C:\Documents and Settings\user\Desktop\pyinstaller-2.0\pyinstaller.py" -F -w -i data\ixi_transp.ico slicer.py
-
-MOVE dist\slicer.exe "C:\Documents and Settings\user\Desktop\slicer_dist"
-
-
-RMDIR /S /Q build
-RMDIR /s /Q dist 
-
--- pack.sh
+++ pack.sh
@@ -1,3 +1,10 @@
-python Configure.py
-python -O /home/r2d2/Mahaigaina/pyinstaller-1.5.1/Makespec.py -F -w -i data\ixi_transp.ico slicer.py 
-python -O /home/r2d2/Mahaigaina/pyinstaller-1.5.1/Build.py optimize=True /home/r2d2/Mahaigaina/pyinstaller-1.5.1/slicer/slicer.spec  
+#python Configure.py
+#python -O /home/r2d2/Mahaigaina/pyinstaller-1.5.1/Makespec.py -F -w -i data\ixi_transp.ico slicer.py 
+#python -O /home/r2d2/Mahaigaina/pyinstaller-1.5.1/Build.py optimize=True /home/r2d2/Mahaigaina/pyinstaller-1.5.1/slicer/slicer.spec 
+
+python -O /home/r2d2/Mahaigaina/PyInstaller-2.1/pyinstaller.py -F -w -d -c -i ./data\ixi_transp.ico slicer.py
+
+cp -r ./dist/* ~/Mahaigaina/dist
+
+rm -rf ./dist
+rm -rf ./build
